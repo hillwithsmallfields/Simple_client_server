47a290f6df504b6bbbd7abc8bc7ba7c4b52475cc
John Sturdy <jcg.sturdy@gmail.com>
2019-03-20 20:54:10 +0000

    Re-arrange to pass the server function in as an argument to the inner main

--- client_server.py
+++ client_server.py
@@ -11,41 +11,77 @@ import socket
 import socketserver
 import threading
 
-def read_csv_as_lists(filename):
+def read_csv_as_dicts(filename, keyfield):
     data = {}
-    with io.open(filename, 'r', encoding='utf-8') as input:
-        data_reader = csv.reader(input)
+    with io.open(filename, 'r', encoding='utf-8') as instream:
+        data_reader = csv.DictReader(instream)
         for row in data_reader:
-            data[row[0]] = row
+            data[row[keyfield]] = row
+    return data
+
+def read_csv_as_lists(filename, keycolumn=0):
+    data = {}
+    with io.open(filename, 'r', encoding='utf-8') as instream:
+        data_reader = csv.reader(instream)
+        for row in data_reader:
+            data[row[keycolumn]] = row
     return data
 
 class simple_data_server():
 
-    def __init__(self, files):
+    def __init__(self, host, port, get_result, files):
+        self.host = host
+        self.port = port
         self.files_readers = files
+        # todo: put the timestamps into a separate dictionary
         self.files_data = {filename: (0, None) for filename in files.keys()}
+        self.udp_server=service_thread(
+            args=[socketserver.UDPServer((self.host, self.port),
+                                         MyUDPHandler)],
+            server=self,
+            get_result=get_result)
+        self.tcp_server = service_thread(
+            args=[socketserver.TCPServer((self.host, self.port),
+                                         MyTCPHandler)],
+            server=self,
+            get_result=get_result)
+
+    def start(self):
+        self.udp_server.start()
+        self.tcp_server.start()
 
     def check_data_current(self):
         for filename, timestamped_data in self.files_data.items():
             timestamp = os.path.getctime(filename)
             if timestamp > timestamped_data[0]:
+                reader = self.files_readers.get(filename, None)
+                if isinstance(reader, str):
+                    key = reader
+                    reader = read_csv_as_dicts
+                elif isinstance(reader, int):
+                    key = reader
+                    reader = read_csv_as_lists
                 self.files_data[filename] = (
                     timestamp,
-                    (self.files_readers.get(filename, None)
-                     or read_csv_as_lists)(filename))
-
-    def get_result(self, input):
-        """Return the result corresponding to the input argument."""
-        self.check_data_current()
-        return ",".join(self.files_data[main_filename][1].get(input,
-                                                              [None, "unknown"])[1:])
+                    reader(filename, key))
 
 main_filename = "/var/local/demo/demo-main.csv"
 
-my_server = simple_data_server({main_filename: None})
+class service_thread(threading.Thread):
+    """A wrapper for threads, that passes in a service function."""
 
-def get_result(query):
-    return my_server.get_result(query)
+    def __init__(self,
+                 server,
+                 get_result,
+                 **rest):
+        super().__init__(target=run_server,
+                         **rest)
+        self.server = server
+        self.get_result = get_result
+
+    def get_result(self, data_in):
+        """Return the result corresponding to the input argument."""
+        return self._get_result(data_in, self.server.files_data)
 
 class MyTCPHandler(socketserver.StreamRequestHandler):
 
@@ -58,11 +94,12 @@ class MyTCPHandler(socketserver.StreamRequestHandler):
         # self.service = service
 
     def handle(self):
-        self.data = self.rfile.readline().strip()
-        # todo: I want to call self.service.get_result, but what is passed when MyTCPHandler is specified is the class object, not an instance, so the service can't be passed in for creating it
-        # todo: probably put it in thread local storage https://www.bogotobogo.com/python/Multithread/python_multithreading_Thread_Local_Specific_Data.php and get it via threading.current_thread()
-        # todo: or maybe I can subclass Thread and put it in there
-        self.wfile.write(bytes(get_result(self.data.decode('utf-8')),
+        my_thread = threading.current_thread()
+        my_thread.server.check_data_current()
+        my_server = my_thread.server
+        self.wfile.write(bytes(my_thread.get_result(
+            self.rfile.readline().strip().decode('utf-8'),
+            my_server.files_data),
                                'utf-8'))
 
 class MyUDPHandler(socketserver.BaseRequestHandler):
@@ -76,23 +113,29 @@ class MyUDPHandler(socketserver.BaseRequestHandler):
         # self.service = service
 
     def handle(self):
-        data = self.request[0].strip()
-        socket = self.request[1]
-        socket.sendto(bytes(get_result(data.decode('utf-8')),
+        reply_socket = self.request[1]
+        my_thread = threading.current_thread()
+        my_server = my_thread.server
+        my_server.check_data_current()
+        reply_socket.sendto(bytes(my_thread.get_result(
+            self.request[0].strip().decode('utf-8'),
+            my_server.files_data),
                             'utf-8'),
                       self.client_address)
 
 def run_server(server):
     server.serve_forever()
 
-def run_servers(host, port):
-    threading.Thread(target=run_server,
-                     args=[socketserver.UDPServer((host, port),
-                                                  MyUDPHandler)]).start()
+def sample_getter(in_string, files_data):
+    print("sample_getter with args", in_string, files_data)
+    return ";".join(in_string.split())
 
-    threading.Thread(target=run_server,
-                     args=[socketserver.TCPServer((host, port),
-                                                  MyTCPHandler)]).start()
+def run_servers(host, port, getter, files):
+    my_server = simple_data_server(
+        host, port,
+        getter,
+        files)
+    my_server.start()
 
 def get_response(query, host, port, tcp=False):
     if tcp:
@@ -110,7 +153,24 @@ def get_response(query, host, port, tcp=False):
         received = str(sock.recv(1024), "utf-8")
     return received
 
-def main():
+def client_server_main(getter, files):
+    """Run a simple client or server.
+
+The argument `getter' is a function to be used by the server to
+process the data it gets from the client.  It should take two
+arguments, a string and a dictionary binding filenames to the result
+of the reader functions for those file (see below), and return the
+result string.
+
+The argument `files' is a dictionary binding filenames to functions
+for reading them.  The reading functions may be functions of two args,
+one the filename and the other a key, or a tuple of the function and
+the key, or a string, in which case it is used as the key for a
+built-in reader function using csv.DictReader, or a number, in which
+case it is used as the key for a built-in reader function using
+csv.reader.
+
+    """
     parser=argparse.ArgumentParser()
     parser.add_argument('--host', '-H', default="127.0.0.1")
     parser.add_argument('--port', '-P', default=9999)
@@ -125,7 +185,9 @@ def main():
                         help="""The data to send to the server.""")
     args=parser.parse_args()
     if args.server:
-        run_servers(args.host, int(args.port))
+        run_servers(args.host, int(args.port),
+                    getter=getter,
+                    files=files)
     else:
         text = " ".join(args.data[0])
 
@@ -133,6 +195,10 @@ def main():
 
         print("Sent:     {}".format(text))
         print("Received: {}".format(received))
+
+def main():
+    client_server_main(sample_getter,
+                       {main_filename: 0})
 
 if __name__ == "__main__":
     main()
