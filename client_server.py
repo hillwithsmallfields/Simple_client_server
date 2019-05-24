5f72af778be182a0903ed542118b261f47652301
John Sturdy <jcg.sturdy@gmail.com>
2019-05-24 21:26:29 +0200

    Remove imports that are no longer used

--- client_server.py
+++ client_server.py
@@ -38,7 +38,7 @@
 # - https://docs.python.org/3.2/library/socketserver.html
 # - https://stackoverflow.com/questions/28426102/python-crypto-rsa-public-private-key-with-large-file/28427259
 
-from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.Cipher import AES
 from Crypto.PublicKey import RSA
 from Crypto import Random
 import argparse
@@ -47,7 +47,6 @@ import csv
 import decouple
 import io
 import os
-import re
 import socket
 import socketserver
 import stat
